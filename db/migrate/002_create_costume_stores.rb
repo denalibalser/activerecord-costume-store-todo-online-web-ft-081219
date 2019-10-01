42f05385eb2d6cbc646f6cde3bb0be674ad03f2d
Denali Balser <denali.balser167@gmail.com>
2019-10-01 16:29:27 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,8 @@
 class CostumeStore < ActiveRecord::Migration[5.2]
-  
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      
 end
