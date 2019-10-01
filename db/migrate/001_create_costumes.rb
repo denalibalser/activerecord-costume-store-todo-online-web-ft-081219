8978d06ef18c56c06904fa4eb9c4ca34ccdb0354
Denali Balser <denali.balser167@gmail.com>
2019-10-01 16:26:17 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,7 +15,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.integer :price
       t.string :image_url
       t.string :size
-      t.datetime :created_at
+      t.timestamps
     end
   end
 
