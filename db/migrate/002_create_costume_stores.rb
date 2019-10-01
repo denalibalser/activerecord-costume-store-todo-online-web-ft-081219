b92ecf494f7164d01dfe28b9e0613767d661e465
Denali Balser <denali.balser167@gmail.com>
2019-10-01 16:39:13 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,4 @@
-class CostumeStore < ActiveRecord::Migration[5.2]
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
@@ -9,5 +9,5 @@ class CostumeStore < ActiveRecord::Migration[5.2]
       t.datetime :opening_time
       t.datetime :closing_time
     end
-  end 
+  end
 end
