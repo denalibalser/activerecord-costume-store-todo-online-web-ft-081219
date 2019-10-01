07c6c1f55380223ebdd812ff9202d6858fe1b16c
Denali Balser <denali.balser167@gmail.com>
2019-10-01 16:44:06 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,14 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
-  
+  def change
+    creat_table :hauntedhouses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :description
+    end
+  end 
 end
